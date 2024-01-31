3e298775a2e572a46bde968167a5dd709cb07469
Dennis Frett <dennis.frett@live.com>
2024-01-31 19:56:08 +0100

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,55 +5,109 @@
 #define MOVEMENT_PIN A0
 #define SERVO_PIN 5
 #define LED_PIN 6
+#define BUZZER_PIN 2
 
 #define MIN_SERVO_ANGLE 75
 #define MAX_SERVO_ANGLE 180
+#define START_SERVO_ANGLE ((MAX_SERVO_ANGLE + MIN_SERVO_ANGLE) / 2)
+
+#define AUTO_ON_AFTER_WAIT true
+
+class MovementBuzzer {
+public:
+  void Trigger(long currentTime) {
+    // Don't trigger too often.
+    if (currentTime - buzzerOnTime < 500) {
+      return;
+    }
+
+    buzzerOnTime = currentTime;
+    buzzerOn = true;
+  }
+  void Tick(long currentTime) {
+    if (!buzzerOn) {
+      digitalWrite(BUZZER_PIN, LOW);
+
+      return;
+    }
+
+    digitalWrite(BUZZER_PIN, HIGH);
+
+    if (currentTime - buzzerOnTime > 50) {
+      buzzerOn = false;
+    }
+  }
+
+private:
+  long buzzerOnTime;
+  bool buzzerOn = false;
+};
+
+class Movement {
+public:
+  bool IsDown(long currentTime) {
+    const auto isDown = analogRead(MOVEMENT_PIN) < 200;
+    if (isDown) {
+      lastDown = currentTime;
+    }
+
+    return isDown;
+  }
+
+  long lastDown = 0;
+};
+
+Movement movement;
 
 class Motor {
 public:
   void Init() {
     servo.attach(SERVO_PIN);
-    servo.write(MIN_SERVO_ANGLE);
+    servo.write(START_SERVO_ANGLE);
   }
 
   void Tick(long currentTime) {
 
     SetState(currentTime);
 
-    if (currentTime - timeOutStart < 2000) {
-      digitalWrite(LED_PIN, HIGH);
-
+    if (currentTime - timeOutStart < 500) {
       return;
     }
 
     isTimedOut = false;
-
-    digitalWrite(LED_PIN, LOW);
 
     if (!IsOn()) {
       return;
     }
 
     static long lastMove = 0;
-    if (currentTime - lastMove < 300) {
+    if (currentTime - lastMove < 15) {
       return;
     }
 
     lastMove = currentTime;
 
     if (goingUp) {
-      servo.write(MAX_SERVO_ANGLE);
-      goingUp = false;
+      angle += angleIncrement;
     } else {
-      servo.write(MIN_SERVO_ANGLE);
+      angle -= angleIncrement;
+    }
+
+    if (angle > MAX_SERVO_ANGLE) {
+      angle = MAX_SERVO_ANGLE;
+      goingUp = false;
+    } else if (angle < MIN_SERVO_ANGLE) {
+      angle = MIN_SERVO_ANGLE;
       goingUp = true;
     }
+
+    servo.write(angle);
   }
 
   void TurnOn(long currentTime) {
     shouldMove = true;
     turnOnTime = currentTime;
-    onDuration = random(5000, 10000);
+    onDuration = random(25000, 30000);
   }
 
   void TurnOff(long currentTime) {
@@ -74,6 +128,28 @@ public:
 
 private:
   void SetState(long currentTime) {
+    const auto isDown = movement.IsDown(currentTime);
+
+    if (!IsOn() && isDown) {
+      TurnOn(currentTime);
+      return;
+    }
+
+    if (IsOn() && isDown) {
+      //    Timeout(currentTime);
+      angleIncrement = 7;
+    }
+
+    if (IsOn() && !isDown) {
+      angleIncrement = 2;
+    }
+
+    // Go to standby after 30s.
+    if (currentTime - movement.lastDown > 300000) {
+      TurnOff(currentTime);
+      return;
+    }
+
     // Turn off after certain time.
     if (IsOn() && (currentTime - turnOnTime > onDuration)) {
       Serial.println("Turning off after " + String(onDuration / 1000) + "s");
@@ -82,7 +158,7 @@ private:
     }
 
     // Turn on again after a while.
-    if (!IsOn() && (currentTime - turnOffTime > 20000)) {
+    if (AUTO_ON_AFTER_WAIT && !IsOn() && (currentTime - turnOffTime > 20000)) {
       Serial.println("Turning on again after 20s");
       TurnOn(currentTime);
     }
@@ -94,45 +170,51 @@ private:
   bool shouldMove = true;
 
   bool goingUp = true;
+  int angle = START_SERVO_ANGLE;
 
   long timeOutStart = 0;
+
+  int angleIncrement = 1;
 
   Servo servo;
 };
 
 Motor motor;
+MovementBuzzer buzzer;
 
-void HandleMotor(long currentTime) {
-  static long lastMovementStart = 0;
-  static long downCount = 0;
+// void HandleMotor(long currentTime) {
+//   const bool movementDetected = analogRead(MOVEMENT_PIN) > 500;
 
-  const bool movementDetected = analogRead(MOVEMENT_PIN) > 500;
+//   if (movementDetected) {
+//     buzzer.Trigger(currentTime);
+//   }
 
-  if (!motor.isTimedOut && motor.IsOn() &&
-      currentTime - motor.turnOnTime > 1000) {
-    if (movementDetected) {
-      Serial.println("Timing out");
-      motor.Timeout(currentTime);
-    }
-  }
+//   if (!motor.isTimedOut && motor.IsOn() &&
+//       currentTime - motor.turnOnTime > 1000) {
+//     if (movementDetected) {
+//       Serial.println("Timing out");
+//       motor.Timeout(currentTime);
+//     }
+//   }
 
-  if (!movementDetected) {
-    return;
-  }
+//   if (!movementDetected) {
+//     return;
+//   }
 
-  // Only turn on motor when it's off.
-  if (motor.IsOn()) {
-    return;
-  }
+//   // Only turn on motor when it's off.
+//   if (motor.IsOn()) {
+//     return;
+//   }
 
-  motor.TurnOn(currentTime);
-}
+//   motor.TurnOn(currentTime);
+// }
 
 void setup() {
   Serial.begin(115200);
   motor.Init();
 
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   pinMode(MOVEMENT_PIN, INPUT_PULLUP);
 
   Serial.println("Beginning!");
@@ -141,8 +223,12 @@ void setup() {
 void loop() {
   const auto currentTime = millis();
 
-  HandleMotor(currentTime);
+  // HandleMotor(currentTime);
+
   motor.Tick(currentTime);
+
+  // Don't use buzzer for now.
+  // buzzer.Tick(currentTime);
 
   digitalWrite(LED_BUILTIN, motor.isTimedOut);
 }
